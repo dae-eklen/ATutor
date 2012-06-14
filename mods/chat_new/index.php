bf42421435b8856c3a0dee0d590edd37f2c07328
dae-eklen <dae.eklen@gmail.com>
2012-06-14 22:06:51 +0300

    interface: conversations tab ready

--- mods/chat_new/index.php
+++ mods/chat_new/index.php
@@ -90,8 +90,80 @@ require (AT_INCLUDE_PATH.'header.inc.php');
            </ul>
 	</div>
 	
-	<div id="tabs-2"> 
-        <ul id="conversations">
+	<div id="tabs-2">	
+		<div id="subtabs">
+			<ul>
+				<li><a href="#subtabs-1">Conversation 1</a> <span class="ui-icon ui-icon-close">Remove Tab</span></li>
+				<li><a href="#subtabs-2">Conversation 2</a> <span class="ui-icon ui-icon-close">Remove Tab</span></li>
+			</ul>
+			<div id="subtabs-1">
+				<ul class="conversations">
+         			<li class="conversations_item">
+         				<table><tr>
+         					<td>
+                            <div class="picture"></div>
+                        	</td>
+                        	
+                        	<td  class="conversations_middle">
+                        	<label class="conversations_name"><a href="##">Lorem ipsum</a></label>
+                        	<div class="conversations_msg">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
+                        	</td>
+                        	
+                        	<td>
+                        	<span class="conversations_time"><nobr>April 6</nobr></span>                            
+                        	</td>
+                        </tr></table>
+                	</li> 
+                	
+                	<li class="conversations_item">
+         				<table><tr>
+         					<td>
+                            <div class="picture"></div>
+                        	</td>
+                        	
+                        	<td  class="conversations_middle">
+                        	<label class="conversations_name"><a href="##">Lorem ipsum</a></label>
+                        	<div class="conversations_msg">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
+                        	</td>
+                        	
+                        	<td>
+                        	<span class="conversations_time"><nobr>April 6</nobr></span>                            
+                        	</td>
+                        </tr></table>
+                	</li> 
+                	
+                	<li class="conversations_item">
+         				<table><tr>
+         					<td>
+                            <div class="picture"></div>
+                        	</td>
+                        	
+                        	<td  class="conversations_middle">
+                        	<label class="conversations_name"><a href="##">Lorem ipsum</a></label>
+                        	<div class="conversations_msg">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
+                        	</td>
+                        	
+                        	<td>
+                        	<span class="conversations_time"><nobr>April 6</nobr></span>                            
+                        	</td>
+                        </tr></table>
+                	</li>  
+           		</ul>
+				<table class="conversations_table"><tr>
+					<td class="conversations_table_spacer"></td>
+					<td><textarea class="conversations_textarea" name="content"></textarea></td>
+					<td class="conversations_table_button"><input class="conversations_send" type="button" label="submit" value="Send"/></td>
+				</tr></table>
+			</div>
+			
+			<div id="subtabs-2">
+				<ul class="conversations">
          			<li class="conversations_item">
          				<table><tr>
          					<td>
@@ -120,7 +192,7 @@ require (AT_INCLUDE_PATH.'header.inc.php');
                         	<td  class="conversations_middle">
                         	<label class="conversations_name"><a href="##">Lorem ipsum</a></label>
                         	<div class="conversations_msg">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 							</div>
                         	</td>
                         	
@@ -128,28 +200,16 @@ require (AT_INCLUDE_PATH.'header.inc.php');
                         	<span class="conversations_time"><nobr>April 6</nobr></span>                            
                         	</td>
                         </tr></table>
-                	</li>  
-                	
-                	<li class="conversations_item">
-         				<table><tr>
-         					<td>
-                            <div class="picture"></div>
-                        	</td>
-                        	
-                        	<td  class="conversations_middle">
-                        	<label class="conversations_name"><a href="##">Lorem ipsum</a></label>
-                        	<div class="conversations_msg">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-							</div>
-                        	</td>
-                        	
-                        	<td>
-                        	<span class="conversations_time"><nobr>April 6</nobr></span>                            
-                        	</td>
-                        </tr></table>
-                	</li>     
-                        
-           </ul>
+                	</li>       
+           		</ul>
+           		<table class="conversations_table"><tr>
+					<td class="conversations_table_spacer"></td>
+					<td><textarea class="conversations_textarea" name="content"></textarea></td>
+					<td class="conversations_table_button"><input class="conversations_send" type="button" label="submit" value="Send"/></td>
+				</tr></table>
+			</div>
+			
+		</div>        		
 	</div>
 	
 	<div id="tabs-3"> 
@@ -162,7 +222,7 @@ require (AT_INCLUDE_PATH.'header.inc.php');
 </div>  
 </div>
 <script>
-    jQuery('#tabs').tabs();
+    jQuery("#tabs, #subtabs").tabs();
 </script>
 
 <?php require (AT_INCLUDE_PATH.'footer.inc.php'); ?>
--- mods/chat_new/js/interface.js
+++ mods/chat_new/js/interface.js
@@ -1,5 +1,5 @@
 
-
+// ================= inbox list
 function read(chbox){
 	// console.log(chbox);
 	var clickedChbox = document.getElementById(chbox);
@@ -30,3 +30,28 @@ function findParentNode(parentClass, childObj) {
     // console.log(testObj);
     return testObj;
 }
+
+
+// ================= conversation tabs
+jQuery(function() {
+		var jQuerytab_title_input = jQuery( "#tab_title"),
+			jQuerytab_content_input = jQuery( "#tab_content" );
+		var tab_counter = 2;
+
+		// tabs init with a custom tab template and an "add" callback filling in the content
+		var jQuerysubtabs = jQuery( "#subtabs").tabs({
+			tabTemplate: "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close'>Remove Tab</span></li>",
+			add: function( event, ui ) {
+				var tab_content = jQuerytab_content_input.val() || "Tab " + tab_counter + " content.";
+				jQuery( ui.panel ).append( "<p>" + tab_content + "</p>" );
+			}
+		});
+
+		// close icon: removing the tab on click
+		// note: closable tabs gonna be an option in the future - see http://dev.jqueryui.com/ticket/3924
+		jQuery( "#subtabs span.ui-icon-close" ).live( "click", function() {
+			var index = jQuery( "li", jQuerysubtabs ).index( jQuery( this ).parent() );
+			jQuerysubtabs.tabs( "remove", index );
+		});
+	});
+	
